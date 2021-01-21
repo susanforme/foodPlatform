4229fd9c4ad0213aa47372315a3e16a984dac6d7
Spring  Breeze <rzc307853639@gmail.com>
2021-01-21 16:32:00 +0800

    error

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,8 @@ import configurations from '../bin/config';
 import { FoodServer, NODE_ENV } from '@/types';
 import responseCachePlugin from 'apollo-server-plugin-response-cache';
 import { PATH_ENV } from './plugins';
-import bodyPareser from 'body-parser';
-import { getMySession } from './config';
 import { context } from './document/context';
+import { setConfig } from './config';
 
 const cert = readFileSync(join(__dirname, '../cert/cert.pem'));
 const key = readFileSync(join(__dirname, '../cert/key.pem'));
@@ -38,22 +37,20 @@ if (config.ssl) {
   server = http.createServer(app);
 }
 
-const connection = mongoose.createConnection('mongodb://localhost:27017/food', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  user: PATH_ENV.DATA_BASE_FOOD_ACCOUNT,
-  pass: PATH_ENV.DATA_BASE_FOOD_PASSWORD,
-});
-
-connection.then(() => {
-  app.use(bodyPareser.json());
-  app.use(getMySession(connection));
-
-  server.listen(config.port, () => {
-    console.log(
-      `服务器已经成功启动在http${config.ssl ? 's' : ''}://${config.hostname}:${
-        config.port
-      }/graphql`,
-    );
+mongoose
+  .connect('mongodb://localhost:27017/food', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    user: PATH_ENV.DATA_BASE_FOOD_ACCOUNT,
+    pass: PATH_ENV.DATA_BASE_FOOD_PASSWORD,
+  })
+  .then(() => {
+    setConfig(app);
+    server.listen(config.port, () => {
+      console.log(
+        `服务器已经成功启动在http${config.ssl ? 's' : ''}://${config.hostname}:${
+          config.port
+        }/graphql`,
+      );
+    });
   });
-});
