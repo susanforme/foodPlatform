463153164b167d97ad95e06c2d8ff6cdcde3ec98
Spring  Breeze <rzc307853639@gmail.com>
2021-02-18 15:11:29 +0800

    真实ip

--- src/document/context.ts
+++ src/document/context.ts
@@ -3,7 +3,7 @@ import e from 'express';
 
 export function context(context: ExpressContext) {
   const { req } = context,
-    result = req.ip.match(/\d+\.\d+\.\d+\.\d+/),
+    result = (req.socket.remoteAddress || req.ip).match(/\d+\.\d+\.\d+\.\d+/),
     ip = result || '',
     session = req.session;
   return {
